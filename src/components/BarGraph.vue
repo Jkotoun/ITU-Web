3f63b231bafc04f1ade2df6859fc09503c966020
Jiří Vlasák <jirivlasak9@gmail.com>
2020-11-29 16:23:14 +0100

    Added chart

---
+++ src/components/BarGraph.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+import { Component, Mixins, Prop, Watch } from "vue-property-decorator";
+import { Bar } from "vue-chartjs";
+@Component
+export default class BarGraph extends Mixins(Bar) {
+  @Prop({ required: true }) data: any;
+  mounted() {
+    this.drawChart();
+  }
+  @Watch("data")
+  drawChart() {
+    const options = {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              stepSize: 1,
+            },
+          },
+        ],
+      },
+    };
+    this.renderChart(this.data, options);
+  }
+}
+</script>
+
--- src/views/UserPage.vue
+++ src/views/UserPage.vue
@@ -5,7 +5,17 @@
       <b-col>
         <UserInfo class="info" :user="user" />
       </b-col>
-      <b-col> </b-col>
+      <b-col>
+        <b-row>
+          <b-col>
+            <BarGraph v-if="loaded" :data="escortEventsCountData" />
+          </b-col>
+          <b-col> </b-col>
+          <b-col> </b-col>
+          <b-col> </b-col>
+        </b-row>
+        <b-row> </b-row>
+      </b-col>
     </b-row>
     <b-row>
       <b-col>
@@ -25,16 +35,19 @@ import { Component, Vue, Watch } from "vue-property-decorator";
 import { GetUser, User, Event, GetUserEvents } from "../modules/ApiModel";
 import UserInfo from "../components/UserInfo.vue";
 import EventInfo from "../components/EventInfo.vue";
+import BarGraph from "../components/BarGraph.vue";
 
 @Component({
   components: {
     UserInfo,
     EventInfo,
+    BarGraph,
   },
 })
 export default class UserPage extends Vue {
   public user: User = {} as User;
   public events: Event[] = [];
+  public loaded = false;
 
   async created() {
     this.LoadData();
@@ -43,8 +56,13 @@ export default class UserPage extends Vue {
   @Watch("$route")
   async LoadData() {
     const userId = parseInt(this.$route.params.id);
-    GetUser(userId).then((user) => (this.user = user));
-    GetUserEvents(userId).then((events) => (this.events = events));
+    Promise.all([GetUser(userId), GetUserEvents(userId)]).then(
+      ([user, events]) => {
+        this.user = user;
+        this.events = events;
+        this.loaded = true;
+      },
+    );
   }
 
   get futureEvents(): Event[] {
@@ -52,6 +70,22 @@ export default class UserPage extends Vue {
   }
   get pastEvents(): Event[] {
     return this.events.filter((a) => new Date(a.startDate) < new Date());
+  }
+
+  get escortEventsCountData(): any {
+    const escortCount = this.events.filter((a) => a.escortId == this.user.id)
+      .length;
+    const pilotCount = this.events.filter((a) => a.pilotId == this.user.id)
+      .length;
+    return {
+      labels: ["Jako doprovod", "Jako pilot"],
+      datasets: [
+        {
+          data: [escortCount, pilotCount],
+          borderWidth: 1,
+        },
+      ],
+    };
   }
 }
 </script>
